33acec92391334a75e91978e5fc9fd9b3d0c306e
Jerome Lester <ragnaroksixx@live.com>
2018-12-01 15:22:32 -0600

    Food objects and Food Manager

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Food", menuName = "Food/Make Food", order = 2)]
+public class Food : ScriptableObject
+{
+    public string foodName;
+    public GameObject foodPrefab;
+    public Mesh foodMesh;
+}
