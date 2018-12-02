2c6e342d9bd2a4abc6618f1bdffc2b9cf49ade1a
Jerome Lester <ragnaroksixx@live.com>
2018-12-01 19:02:56 -0600

    Added customer food types

--- Assets/Scripts/FoodManager.cs
+++ Assets/Scripts/FoodManager.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class FoodManager : MonoBehaviour
 {
 
-    public List<Food> allFoods;
+    List<Food> allFoods;
+    public List<Food> customerFoods;
+    public List<Food> godFoods;
     public static FoodManager Instance;
     void Awake()
     {
+        allFoods = new List<Food>();
+        foreach (Food f in customerFoods)
+        {
+            allFoods.Add(f);
+        }
+        foreach (Food f in godFoods)
+        {
+            allFoods.Add(f);
+        }
         Instance = this;
     }
 
     public static Food GetRandomFood()
     {
         return Instance.allFoods[Random.Range(0, Instance.allFoods.Count)];
+    }
+    public static Food GetRandomCustomerFood()
+    {
+        return Instance.customerFoods[Random.Range(0, Instance.customerFoods.Count)];
+    }
+    public static Food GetRandomGodFood()
+    {
+        return Instance.godFoods[Random.Range(0, Instance.godFoods.Count)];
     }
 }
