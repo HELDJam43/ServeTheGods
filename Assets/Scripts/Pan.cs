d8961edcdbd75a3a5bb092f89e983b933863879d
Mark T <BluShine@users.noreply.github.com>
2018-12-01 21:43:37 -0600

    Added a stove and frying pans
    Added a stove and fully animated frying pans.

---
+++ Assets/Scripts/Pan.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pan : MonoBehaviour {
+
+    public bool activated = false;
+
+    float activeRamp = 0;
+    static float ACTIVESPEED = 3f;
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(activated)
+        {
+            activeRamp += ACTIVESPEED * Time.deltaTime;
+        } else
+        {
+            activeRamp -= ACTIVESPEED * Time.deltaTime;
+        }
+        activeRamp = Mathf.Clamp01(activeRamp);
+
+        animator.SetFloat("Frying", activeRamp);
+	}
+}
