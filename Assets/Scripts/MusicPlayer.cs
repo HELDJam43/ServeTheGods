cfb8c74fd9b8dc9847d5cbfa9bf1dca0e7853bc1
Mark T <BluShine@users.noreply.github.com>
2018-12-03 19:22:14 -0600

    Added music.
    Added music to the game.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    AudioSource audio;
+    static MusicPlayer instance = null;
+
+	// Use this for initialization
+	void Start () {
+        audio = GetComponent<AudioSource>();
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            instance = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
